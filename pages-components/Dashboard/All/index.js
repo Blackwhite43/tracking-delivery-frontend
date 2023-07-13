bc69e53389c052d744cc5a6ba83e70638159286a
Blackwhite43 <willy.liu.cia.chen@gmail.com>
2023-07-13 13:23:30 +0700

    Add verify all button

--- pages-components/Dashboard/All/index.js
+++ pages-components/Dashboard/All/index.js
@@ -20,6 +20,26 @@ const AllDelivery = () => {
       array.pop(event.target.id);
     }
   }
+  const handleVerifyAll = (event) => {
+    delivery.map(index => {
+      array.push(index.delivery_update._id);
+    })
+    try {
+      array.map(index => {
+        axios.patch(`${process.env.URL}/api/v1/admin/${index}`, {
+          verification: "Verified by Delivery Team"
+        })
+      })
+    } catch (error) {
+      console.log(error);
+    }
+    finally {
+      alert("success");
+      router.reload({
+        pathname: "/delivery"
+      });
+    }
+  }
   const handleVerify = (event) => {
     try {
       array.map(index => {
@@ -260,19 +280,19 @@ const AllDelivery = () => {
     },
     {
       name: "delivery_update",
-      label: "Verification",
       options: {
-        // customHeadRender: (() => {
-        //   return (
-        //     <TableCell>
-        //       <FormControlLabel
-        //         control={<Checkbox />}
-        //         label= "Check All"
-        //         labelPlacement="end"
-        //       />
-        //     </TableCell>
-        //   )
-        // }),
+        customHeadRender: (() => {
+          return (
+            <TableCell>
+              <button
+                onClick={handleVerifyAll}
+                class="btn-rounded text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Verify All Delivery
+              </button>
+            </TableCell>
+          )
+        }),
         customBodyRender: (value) => {
           if (value.verification == "Verified by Delivery Team") {
             return (
